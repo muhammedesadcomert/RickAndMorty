17a3d9c13f454f656aa317d971640326c9a00ff7
Muhammed Esad Cömert <esadcmrt@gmail.com>
2023-03-23 11:57:39 +0300

    Initial commit

---
+++ app/src/main/java/com/invio/rickandmorty/ui/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.invio.rickandmorty.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.invio.rickandmorty.ui.theme.RickAndMortyTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RickAndMortyTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
